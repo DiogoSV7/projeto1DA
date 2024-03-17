6d554835dc400cb3cd35b52ab3b9684c4b42e537
Diogo Vieira <diogovieira@Air-de-Diogo-3.home>
2024-03-17 14:45:46 +0000

    Corrected EdmondsKarp and added super source and super sink

--- includes/Pipes.h
+++ includes/Pipes.h
@@ -11,6 +11,8 @@ class Pipes{
         const std::string getCitySinkName() const;
         const int getCapacity() const;
         const int getDirection() const; //1-> unidirectional 0-> bidirectional
+        bool operator==(const Pipes& other) const;
+        bool operator!=(const Pipes& other) const;
     private:
         std::string serv_point_a_;
         std::string serv_point_b_;
@@ -18,5 +20,16 @@ class Pipes{
         int direction_;
 };
 
-
+namespace std {
+    template<>
+    struct hash<Pipes> {
+        size_t operator()(const Pipes& obj) const {
+            size_t hash1 = std::hash<std::string>()(obj.getCitySourceName());
+            size_t hash2 = std::hash<std::string>()(obj.getCitySinkName());
+            size_t hash3 = std::hash<int>()(obj.getCapacity());
+            size_t hash4 = std::hash<int>()(obj.getDirection());
+            return hash1 ^ (hash2 << 1) ^ (hash3 << 2) ^ (hash4 << 3); // Combining hashes
+        }
+    };
+}
 #endif //PROJETO1DA_PIPES_H
